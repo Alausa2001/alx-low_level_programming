1fe05237e78c1212e7290ce4a3a0183051b35ff2
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-07 15:00:06 -0700

    y first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
+/* My first program */
 #include <stdio.h>
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
+  puts (""Programming is like building a multilingual puzzle\n");
   return(0);
 }	
