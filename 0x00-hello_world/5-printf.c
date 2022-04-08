9ff3a361242de8b53065e03e4d2aea0847b6cfe1
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-07 17:48:58 -0700

    betty nah witch

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,5 +4,5 @@
  * Return: always 0 (zero)
  */
 	int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,\n"); 
+{printf("with proper grammar, but the outcome is a piece of art,\n"); 
 	return (0); }
