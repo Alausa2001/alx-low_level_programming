88ab688fd227a456579ad5b4b1bf7538595cd0f0
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-10 16:03:20 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ double k;
 float c;
 long d;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(j));
+printf("Size of a char: %lu byte(s)\n",(unsigned long)(i));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(j));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
