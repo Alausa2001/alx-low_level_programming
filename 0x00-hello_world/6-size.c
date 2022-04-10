92e7893c94e8d5a690f0a828d7c947fcbcc8fcf5
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-10 15:55:57 -0700

    tast

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int j;
 double k;
 float c;
 long d;
+
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(j));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
