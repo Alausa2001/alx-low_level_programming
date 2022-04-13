b8961056441921dfac3f44355635fc3e417f681a
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 16:01:59 -0700

    done

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,8 +6,8 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || ( c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
-		return(0);
+		return (0);
 }
