92b2c9bf19231ebf5e811df047c3abf09782934d
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 09:45:23 -0700

    please

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_INCLUDED
+#define MAIN_INCLUDED
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,3 +16,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void print_times_table(int n);
 
+#endif
