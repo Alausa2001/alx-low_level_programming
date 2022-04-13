9b05d19564df1f4dd67c79b43701e5eebcae28a1
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 14:20:13 -0700

    progress

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - print alphabet in lower case
+ * print_alphabet - print alphabet in lower case
  *
  * Return: void
  * print_alphabet: lower cases only ' found in main.h
