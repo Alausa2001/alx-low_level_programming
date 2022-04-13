d6bfb8a55e190519224e03e940bba96f58e17491
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 15:43:13 -0700

    be nice

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main"
+/**
+ * _isalpha - checks for alphabet
+ * @c: the character i wan check
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' || 'A' && c <= 'z' || 'Z')
+		return (1);
+	else
+		return(0);
+}
