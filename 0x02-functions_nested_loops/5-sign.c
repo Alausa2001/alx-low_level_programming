0289708178ee305fb3a5020e55b0d18425ee8b45
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 16:33:16 -0700

    be nice

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main"
+/**
+ * print_sign - print sign of numbers
+ * n: number whos sign is to
+ * be printed
+ * Return: 0 1 0r -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == '0')
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
