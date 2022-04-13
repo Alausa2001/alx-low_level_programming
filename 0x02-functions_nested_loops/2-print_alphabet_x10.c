8ed598c06d5dfc6e72adcb8f6ddf86cc2e5ebef3
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 14:43:47 -0700

    getting started

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - Write a function that prints 10 times the alphabet, in lowercase, followed by a new line
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char b;
+	int a;
+	for (a = '0'; a <= 9; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+			_putchar(b);
+	}
+	_putchar('\n');
+}
