5a337c457d04b95b8f57b1ee31780ab6209d459b
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 16:54:27 -0700

    main

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ *
+ * @a: input integer
+ * @b: input integer
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	_putchar(a * b);
+	return (0);
+}
