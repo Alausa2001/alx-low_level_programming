6f400041559e1fb0f2d856327c2654338e6a3649
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 19:16:16 -0700

    apt

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - check main
+ * @n: an input integer
+ * Description: A function that prints
+ * natural numbers from n to 98
+ * RETURN: VOID
+ */
+void print_to_98(int n)
+{
+	for (n = '0'; n < '99'; n++)
+	{
+		printf(n);
+		{
+			printf(',');
+			printf(' ');
+		}
+	}
+}
