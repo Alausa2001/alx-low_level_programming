409b90bcc367ec24be2f8161fd8b225b0dfc185f
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 09:37:31 -0700

    betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,12 @@
 /**
  * _isupper - checks for upper case letters
  *
+ * @c: character being checked
  * Return: either 0 or 1
  */
 int _isupper(int c)
 {
-	if ((c >= 'a'; && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
