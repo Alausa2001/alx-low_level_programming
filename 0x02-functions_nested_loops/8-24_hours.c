b1cf3fe5dbf3fe871dc3e0db0c3d48b02bd30373
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 18:08:17 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,20 @@
  */
 void jack_bauer(void)
 {
-	int a;
-	for (a = '00:00'; a <= '23:59'; a++)
-		_putchar(a);
+	int a, b;
+
+	for (a = 0; a < 24; a++);
+	{
+		for (b = 0; b <= 59; b++);
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar((':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+		}
+	
+	}
 }
+
