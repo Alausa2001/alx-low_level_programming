32342763e3f38e5ac486563007ec269c166f7400
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 17:49:20 -0700

    taskk

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minutr of a day
+ * @a: day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a;
+	for (a = '00:00'; a <= '23:59'; a++)
+		_putchar(a);
+}
