dc53340e43fe7548fa286f50771d5260e5d6e809
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 20:25:18 -0700

    good

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, j;
+
+	j = i;
+
+	for (i = 0; i <= 1023; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			j += i;
+	}
+printf("%d\n", j);
+return (0);
+}
