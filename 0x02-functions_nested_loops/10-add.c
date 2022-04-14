df93088181ff3c042cc12ee5a2af270e5be98eed
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 18:52:42 -0700

    dope

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
  * add - sum
+ * @a: integer a
+ * @b: integer b
+ * Description: funtion that adds two 
+ * integers and return the result
  * Return: Result
  */
 int add(int a, int b)
