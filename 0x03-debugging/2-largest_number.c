8aac1f089d2277e94762a82f205f1e4b2fe822e9
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 07:19:29 -0700

    brain tasking

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,21 +11,19 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	while (a != b && b != c && a != c)
-	{
-		if (a > b && b > c)
+
+		if (a >= b && b >= c)
 		{
 			largest = a;
 		}
-		else if (b > a && a > c)
+		else if (b >= a && a >= c)
 		{
 			largest = b;
 		}
-		else
+		else if (c >= a && c >= a)
 		{
 			largest = c;
 		}
 
 		return (largest);
-	}
 }
