0aeb0332c4e27b4e6fa6bb16b3275f96ad1ebbd6
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-13 17:00:21 -0700

    port

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - print absolutr value
+ * @n: integer
+ * Return: integer
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
