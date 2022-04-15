745c7c3fc9aaad60a464735990c5a902465b3fa6
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 20:47:52 -0700

    port

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_diagonal - i like diagonals
+ * @n: no of times
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	while (n >= 0)
+	{
+		_putchar('\');
+		n--
+	}
+	_putchar('\n');
+}
