54a0471e06c8498990800fb3f57ec504030e8a45
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 21:35:32 -0700

    pot

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_triangle - triianles
+ *
+ * @size: brb
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	for (size = 0; size > 0; size++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
