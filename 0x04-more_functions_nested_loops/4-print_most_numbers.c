a62a86f6155231c889305e6407584082044ec0d2
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 18:14:08 -0700

    main3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - Write a function that prints the numbers, from 0 to 9
+ * except 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a);
+	}
+
+	_putchar('\n');
+}
