917adbada6482efc94b295690e5b315dd1a7bf9a
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 21:29:10 -0700

    pot

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_square - squares
+ * @size: inpit
+ * Return: void
+ */
+void print_square(int size)
+{
+	for (size = 0; size > 0; size++)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
+
