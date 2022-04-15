aab262d7faf77c55abe2b02c1d41ed009d02c741
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-14 20:26:50 -0700

    good

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print line on terminal
+ * @n: no of lines
+ * Return: void
+ */
+void print_line(int n)
+{
+	int p;
+
+	p = _;
+
+	_putchar(p);
+	_putchar('\');
+	_putchar('n');
+}
