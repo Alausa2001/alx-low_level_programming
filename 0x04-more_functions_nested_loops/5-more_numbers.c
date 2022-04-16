b187ea47486d390e92eaeeb30c5cd2df7f75439d
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-15 19:50:47 -0700

    main

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * more_numbers - print 1 to 10 ten times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 15; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
