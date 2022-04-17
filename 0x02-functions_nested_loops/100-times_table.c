b0e3b046fe454f98b582da42c52baa036827efbd
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-17 10:47:23 -0700

    print times teble up to 12 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include "main.h"
+/**
+ * print_times_table - print 0 to 14 times table
+ * @n: input integer
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int row, column, ones, product, tens, hundred, hundreds;
+
+	if (n >= 0 && n < 15)
+	{
+		for (row = 0; row <= 14; row++)
+		{
+			for (column = 0; column <= 14; column++)
+			{
+				product = row * column;
+				tens = product / 10;
+				ones = product % 10;
+				hundred = product / 100;
+				hundreds = product % 100;
+
+				if (column == 0)
+				{
+					_putchar('0');
+				}
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(ones + '0');
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(tens + '0');
+					_putchar(ones + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(hundred + '0');
+					_putchar(hundreds + '0');
+				}
+
+
+			}
+			_putchar('\n');
+		}
+	}
+}
