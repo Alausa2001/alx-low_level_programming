2d2842170128535ca11a8c94273ac70109bda05e
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-16 23:27:48 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,13 @@ void times_table(void)
 {
 	int p, q, result;
 
-	p = 0;
-	q = 0;
+	p = '0';
+	q = '0';
 
 	result = p * q;
-	for (; p <= 9; p++)
+	for (; p <= '9'; p++)
 	{
-		for (; q <= 9; q++)
+		for (; q <= '9'; q++)
 		{
 			_putchar(result);
 			_putchar(',');
