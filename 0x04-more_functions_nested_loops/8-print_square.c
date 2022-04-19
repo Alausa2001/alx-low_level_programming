c70fdf9cf0344058d860600b7b0da7c6cbcf80ab
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-19 07:12:36 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,9 +10,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i < size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 1; j < size; j++)
 				_putchar(35);
 		}
 		_putchar('\n');
