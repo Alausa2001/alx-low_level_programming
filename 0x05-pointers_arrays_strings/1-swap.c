57bc270852b719b8ff0cf408416a74a95a0dea5b
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-20 16:14:17 -0700

    swap intergers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - sswap the value of
+ * two integers
+ * @a: input integer (pointer)
+ * @b: integer pointer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int p;
+	int q;
+
+	a = &p;
+	b = &q;
+	*a = q;
+	*b = p;
+}
