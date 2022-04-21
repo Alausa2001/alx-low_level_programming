0b1e0140b85f0188e34851ec0a35e14d29bec1d5
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-21 10:51:57 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: string to be printed
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int lett;
+
+	lett = 0;
+	while (s[lett] != '\0')
+	{
+		lett++
+	}
+	lett--;
+	while (lett >= 0)
+	{
+		_putchar(s[lett]);
+		lett--
+	}
+	_putchar('\n');
+}
