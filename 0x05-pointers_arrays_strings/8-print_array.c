65084d91eaadc3ef73e97eebe9f2b222a0c7f2a9
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-21 17:50:43 -0700

    print integers stored

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print value using pointers
+ * @n: max no of elements
+ * @a: pointer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int lett = 0;
+
+	for (; lett < n; lett++)
+	{
+		if (lett == (n - 1))
+			printf("%d\n", a[lett]);
+		else
+			printf("%d, ", a[lett]);
+	}
+}
