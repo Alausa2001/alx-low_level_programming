1c4065404862d173928f355d1b88c0c732651340
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-21 18:37:06 -0700

    copy strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - copies the content of
+ * one string to another
+ * @src: original
+ * @dest: copy
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int lett++;
+
+	for (lett = 0; src[lett] != '\0'; lett++)
+	{
+		src[lett] = dest[lett];
+	}
+	dest[lett] = '\0';
+	return (dest);
+}
