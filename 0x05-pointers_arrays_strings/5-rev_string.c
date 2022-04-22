60b203015deadf15d0f51bec4f7aa12b5e1ed009
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-22 14:44:37 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - this function reverses a string
+ * @s: string pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int a, b, l;
+
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	a--;
+	b = 0;
+	while (b < a)
+	{
+		l = s[a];
+		s[a] = s[b];
+		l = s[b];
+		b--
+	}
+}
