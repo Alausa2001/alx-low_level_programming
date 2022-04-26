4d3120cd524008af87e1333dde297e84a7b5fd4d
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-25 18:28:11 -0700

    print no

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_number - this function prints an interger
+ * @n: randon no
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i;
+
+	i = 0;
+	for (; i <= n || i >= n; i++)
+		_putchar(i);
+}
