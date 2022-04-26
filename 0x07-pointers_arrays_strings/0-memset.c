994522f4d001e13678d61a971475f7d963158ada
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-26 10:41:22 -0700

    fill memory with constant bytte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - functions fill memory
+ * pointed to by *s with constsnt byte b
+ * @s: space to be filled
+ * @n: no of byte to fill
+ * @b: the constant byte(filling element)
+ * Return: memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		s[i] = b;
+	return (s[i]);
+}
