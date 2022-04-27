b81b314d9cede29e04f31370b992e45249848340
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-27 14:13:14 -0700

    copy string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,8 +2,8 @@
 /**
  * _strspn - function returns the no of bytes
  *  in the initial segment of s which consist only of bytes from accept
- *  @s: pointer to full string
- *  @accept: pointer to substring
+ *  @s: full string
+ *  @accept:  substring to return
  *  Return: length of substring
  */
 unsigned int _strspn(char *s, char *accept)
