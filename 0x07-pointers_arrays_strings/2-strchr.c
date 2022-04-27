719868494d5d39bf7b8802ba8a3c252b3e8898dd
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-26 17:02:58 -0700

    locate char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strchr - locates a character ina string
+ * @s: string
+ * @c: character to locate
+ * Return: i c is found return c else
+ * return null
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == 'c')
+		{
+			break;
+		}
+		return (s[i]);
+
+		else if (s[i] != 'c' && s[i + 1] == '\0')
+			return (NULL);
+	}
+}
