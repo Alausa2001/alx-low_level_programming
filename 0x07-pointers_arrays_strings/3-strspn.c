f4152743d03dc0b848753df3e9fda7a541f8ff1c
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-27 13:57:42 -0700

    return length of substrring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - function returns the no of bytes
+ *  in the initial segment of s which consist only of bytes from accept
+ *  @s: pointer to full string
+ *  @accept: pointer to substring
+ *  Return: length of substring
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0');
+	{
+		i++;
+	}
+	j = 0;
+	while (accept[j] <= s[i])
+	{
+		j++;
+	}
+	return (accept);
+}
