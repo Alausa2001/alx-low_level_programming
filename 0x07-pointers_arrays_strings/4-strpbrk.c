8d02128a3fcde8336283cee3ea605380a14fd283
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-27 20:33:32 -0700

    search for matches

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - searches for a string for aany set of bytes
+ * @s: string to be searched
+ * @accept: contains the matching chars
+ * Return: It returns a pointer to the character in s1 that
+ * that matches char in accept or nu if there is no match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+				return (s);
+			else if (accept[j] != s[i])
+				return (0);
+		}
+	}
+}
