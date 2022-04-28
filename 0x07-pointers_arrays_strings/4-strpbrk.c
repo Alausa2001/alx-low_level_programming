57f0f4af51a619b011b8c33dd1668bc48c487c2c
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-27 20:39:39 -0700

    search for matches

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,8 +14,8 @@ char *_strpbrk(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] == s[i])
-				return (&s[i]);
+			if (s[i] == accept[j])
+				return (s);
 		}
 	}
 }
