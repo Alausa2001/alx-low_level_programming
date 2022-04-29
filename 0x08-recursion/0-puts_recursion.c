63a4ccf677a7a697cb168edb05c779db22649e9d
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-28 18:15:16 -0700

    print dtring recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string recursively
+ * @s: string to print
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		return;
+	}
+	else
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
