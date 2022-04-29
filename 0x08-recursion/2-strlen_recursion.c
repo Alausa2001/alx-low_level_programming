e3a732f5d2b38d908bd9d5b0ce27afd69c84410a
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-28 19:19:12 -0700

    return length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return string length
+ * but do it recursively
+ *
+ * @s: string
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (s[l] == '\0')
+	{
+		return (l);
+	}
+	else if (s[l] != '\0')
+	{
+		_strlen_recursion(l + 1);
+	}
+}
