3cf0638b9019ef85472cae64647e26c43de88972
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-04-28 20:49:13 -0700

    print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,8 +11,8 @@ void _print_rev_recursion(char *s)
 
 	if (s[n] == '\0')
 		return;
-	else if (n > 0)
+	else if (s == s - 1)
 
-		_putchar(s[n]);
+		_putchar(*s);
 	_print_rev_recursion(*s - 1);
 }
