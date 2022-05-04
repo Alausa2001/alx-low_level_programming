ec1595c1dd24cbd8a06d5aa5e5c634102104bcb4
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-03 20:09:50 -0700

    print no of argument passed to the progrm

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function print a number
+ * @agrc: The size of the argv array
+ * @agrv: An array of size argc
+ * Return: 0
+ */
+int main(int agrc, char **agrv)
+{
+	printf("%d\n", agrc);
+
+	return (0);
+}
