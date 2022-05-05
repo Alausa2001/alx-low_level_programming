2727c32ebed2e4e7d22f986d0d96367f1197a241
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-05 16:27:50 -0700

    task 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * create_array - this function creates an array
+ * @size: size of array to create
+ * @c: array name
+ * Return: base address if successful, else null
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size != 0)
+	{
+		c = malloc(size * sizeof(char));
+		return (c);
+	}
+	else if (size == 0)
+		return (NULL);
+}
