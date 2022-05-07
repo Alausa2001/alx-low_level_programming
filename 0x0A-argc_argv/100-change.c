2911dd47f3208e27e33ac78ca187d046fc517b26
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-07 15:41:21 -0700

    complet now

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,6 +13,8 @@ int main(int argc, char **argv)
 	int cents, coins;
 	int i = 1;
 
+	coins = 0;
+
 	cents = atoi(argv[i]);
 
 	if (argc != 2)
@@ -24,5 +26,30 @@ int main(int argc, char **argv)
 	{
 		printf("0\n");
 	}
+	if (cents >= 25)
+	{
+		coins += cents / 25;
+		cents = cents % 25;
+	}
+	if (cents % 10 >= 0)
+	{
+		coins += cents / 10;
+		cents = cents % 10;
+	}
+	if (cents % 5 >= 0)
+	{
+		coins += cents / 5;
+		cents = cents % 5;
+	}
+	if (cents % 2 >= 0)
+	{
+		coins += cents / 2;
+		cents = cents % 2;
+	}
+	if (cents % 1 >= 0)
+	{
+		coins += cents / 1;
+	}
+	printf("%d\n", coins);
+	return (0);
 }
-
