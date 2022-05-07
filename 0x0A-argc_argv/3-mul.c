dbeaa71d5b1898e6119b23c52ade2d64dbc68609
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-07 05:46:02 -0700

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 /**
  * main - program prints multiplies two integers
- * @agrc: no of command line arguments
- * @agrv: array of coomand line arguments
+ * @argc: no of command line arguments
+ * @argv: array of coomand line arguments
  * Return: 1 if it does not receive two arguments
  */
 int main(int argc, char **argv)
 {
 	int i, mul = 1;
 
-	if (argc > 1 && argc < 4)
+	if (argc > 1 && argc == 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
