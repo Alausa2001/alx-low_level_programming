4d1165def896c725ae0986e95caecc759ec4c8fc
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-10 03:14:05 -0700

    Write a function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory, if  the malloc fails
+ * the malloc_checked function should cause normal
+ * process termination with a status value of 98
+ * @b: the size of memory to be allocated
+ * Return: void
+ */
+void *malloc_checked(unsigned int b);
+{
+	char *memory;
+
+	memory = malloc(sizeof(unsigned int) * b);
+
+	if (memory == NULL)
+		exit(98);
+}
