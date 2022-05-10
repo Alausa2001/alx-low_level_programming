f2b21fae44983280546381acbd8d7939496aaf5a
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-10 12:54:10 -0700

    ups

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,9 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i] = s1[i];
 	}
-	for (; i <= (n + len1); i++)
+	for (; j < n; j++, i++)
 	{
-		p[i] = s2[i - len1];
+		p[i] = s2[j];
 	}
 	p[i] = '\0';
 	return (p);
