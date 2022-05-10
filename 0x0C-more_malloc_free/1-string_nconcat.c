4124e3cf7007d6082e180efb1dedcb65346a49d9
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-10 12:14:55 -0700

    concatenate s1 and n part of s2 into an allocated memory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i] = s1[i];
 	}
-	for (j = 0; j <= n + len1; j++)
+	for (j = 0; j <= (n + len1); j++)
 	{
 		p[i] = s2[j - len1];
 	}
