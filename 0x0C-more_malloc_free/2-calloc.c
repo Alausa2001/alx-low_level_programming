9c58729f2fde4566986e8ac030659f2ed6acf4a3
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-10 18:23:48 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,10 +11,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
 
-	p = malloc(nmemb * size);
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	return (p);
 }
