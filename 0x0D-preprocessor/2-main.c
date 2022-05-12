10eb5c7a458f4334303885b6f46c61942e2966cd
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-12 05:03:37 -0700

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 /**
  * main - Write a program that prints the name of the
  *  file it was compiled from, followed by a new line
- *  Return : 0
+ *  Return: 0
  */
 int main(void)
 {
-	printf("%s\n", __FILE__ );
+	printf("%s\n", __FILE__);
 	return (0);
 }
