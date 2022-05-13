e2e70003290e926d5f777606162ebd538adaa800
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-12 21:08:22 -0700

    structures

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_dog - function prints the dog info
+ * @d: is a pointer to dog
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
