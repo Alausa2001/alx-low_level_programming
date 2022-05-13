dfe1620fea56195cd26b4a065df6503f678afdd3
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-12 21:00:19 -0700

    structures

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - function intiailzes the dog info
  * @d: pointer to a dog
@@ -11,7 +12,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
-	d->owner = owner;
 	d->name = name;
 	d->age = age;
+	d->owner = owner;
 }
