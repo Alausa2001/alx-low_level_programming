a5e47a9489ae959b33de0e24c8056f090ab59788
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-12 20:37:50 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#define _DOG_H_
 #ifndef _DOG_H_
+#define _DOG_H_
 /**
  * strucy dog - struct store some info about dogs
  * @name: name of dog
@@ -20,5 +20,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+typedef struct dog dog_t;
 
 #endif
