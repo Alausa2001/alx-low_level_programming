6f52473f91d95f5fb042602e0faf08c1fe190c35
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-16 21:59:10 -0700

    variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("(nil)");
 		if (separator != NULL && i < n - 1)
-			printf("%s", str);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(args);
