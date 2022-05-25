bcd4d740f8517879d761711337a6fe6ee608bca8
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-24 21:28:57 -0700

    return no pf element in list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_list - prints all elements of a list_t list.
+ * list_len - return no elements of a list_t list.
  * @h: pointer to struct list_t
  * Return: the number of nodes
  */
