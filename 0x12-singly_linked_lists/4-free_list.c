29ac671dd5461d19a9450cdb3931e0917e267cb3
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-24 21:37:32 -0700

    return no pf element in list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to list_t.
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *access;
+
+	while (head != NULL)
+	{
+		access = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
