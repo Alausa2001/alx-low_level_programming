27df07ae3f7a8c733e5dec77750bd240ab927f2d
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-28 02:55:42 -0700

    not yet complete

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *newnode;
-	listint_t *trav;
+	listint_t *trav = *head;
 
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
