80fbc73124e58c19d3cc154b702adaadd4ea0428
Alausa2001 <oluwaferanialausagit config --global user.email oluwaferanmialausa2001@gmail.com>
2022-05-28 03:27:10 -0700

    necessary corrections have been made, lets see what happens

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,9 @@
  * listint_t *insert_nodeint_at_index - Write a function that
  * inserts a new node at a given position.
  * @head: it is a pointer to the head of the list
+ * @n: data to be inserted in the newnode
+ * @idx: the index where the node is to be inserted
+ * the first node has an index equal to 0
  * Return: if successful return the newnode else return null
  * if it is impossible to add the new node do not add and
  * return null
@@ -17,6 +20,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (NULL);
+	}
 	idx--;
 	while (idx != 0)
 	{
@@ -28,10 +37,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	newnode->next = trav->next;
 	trav->next = newnode;
 	return (newnode);
-	if (idx == 0)
-	{
-		newnode->next = *head;
-		*head = newnode;
-		return (newnode);
-	}
 }
