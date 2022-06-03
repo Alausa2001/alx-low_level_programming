2a9ca5795c112bc7df35fe0438a7f8e01b70545d
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-03 23:08:53 +0100

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,16 +18,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content)
 	{
-		while (text_content[txt_len])
-			txt_len++;
+		for (txt_len = 0; text_content[txt_len] != '\0'; txt_len++)
+			;
 		characters = write(fd, text_content, txt_len);
 		if (characters == -1)
-		{
-			close(fd);
 			return (-1);
-		}
 	}
 	close(fd);
 	return (1);
