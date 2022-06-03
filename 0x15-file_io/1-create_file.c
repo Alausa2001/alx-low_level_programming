b7f3b22179d15eb6c4409d2e017a1cbcbcd290e9
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-03 21:42:38 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
-#include "main.h"
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include "main.h"
 /**
  * create_file - Create a function that creates a file.
  * @filename: name of the file
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[txt_len] != NULL)
+		while (text_content[txt_len])
 			txt_len++;
 		characters = write(fd, text_content, txt_len);
 		if (characters != txt_len)
