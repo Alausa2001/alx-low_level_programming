e74533d52a82d1ff4370d26844158d11065fe331
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-05 15:36:20 +0100

    corrected to betty coding style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,10 +13,10 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_first = open(argv[1], O_RDONLY);
+	fd_first = open(argv[1], O_RDONLY, 664);
 	if (fd_first == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	fd_second = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	fd_second = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
 	if (fd_second == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	char_read = read(fd_first, buf, 1024);
@@ -27,7 +27,10 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "ERROR: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (char_read == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	closefirst = close(fd_first);
 	if (closefirst == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_first), exit(100);
