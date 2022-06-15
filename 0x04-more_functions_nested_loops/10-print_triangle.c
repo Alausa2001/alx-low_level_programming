7aafc4aca2b4e82884e6419b0360068305e6b05e
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-06-15 23:36:32 +0100

    this func prints rectangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,24 +9,15 @@ void print_triangle(int size)
 {
 	int a, b;
 
-	if (size > 0)
+	if (size >= 0)
 	{
 		for (a = 0; a < size; a++)
 		{
 			for (b = 0; b < size; b++)
 			{
-				if (b < size - a)
-				{
-					_putchar(' ');
-				}
-				else
-					_putchar(35);
+				_putchar(35);
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
