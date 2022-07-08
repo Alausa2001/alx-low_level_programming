577caf23f88a94b14f6620f9199adb12534c882c
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-07-08 21:00:10 +0100

    accounted for if the index is 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = newnode;
 		newnode->prev = NULL;
 	}
-	idx--;	
+	idx--;
 	while (idx != 0 && trav != NULL)
 	{
 		trav = trav->next;
