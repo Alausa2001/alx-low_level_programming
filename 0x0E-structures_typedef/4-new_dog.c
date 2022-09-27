573f230b043593b10003855d6dca5aeabdd7785e
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2022-09-27 10:28:13 +0100

    this fuction create a nw struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
+/**
+ * _strlen - function that prints string length
+ * @s: string whose length is to be returned
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int len;
 
+	len = 0;
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
 /**
  * new_dog - function that creates a new dog
  * @name: name of dog
@@ -11,14 +26,35 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int name_len, owner_len, i, j;
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	new_dog->name = malloc(sizeof(char *) * (_strlen(name) + 1));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	name_len = _strlen(name);
+	for (i = 0; i < name_len; i++)
+		new_dog->name[i] = name[i];
+
 	new_dog->age = age;
-	new_dog->owner = owner;
+
+	owner_len = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char *) * (_strlen(owner) + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (j = 0; j < owner_len; j++)
+		new_dog->owner[i] = owner[i];
 	return (new_dog);
 }
